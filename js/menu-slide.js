d6deee953370d1927be15235b08707aaf760e411
retwedt <retwedt@gmail.com>
2016-03-21 18:30:53 -0500

    initial commit of updated website design

--- js/menu-slide.js
+++
@@ -1,2 +0,0 @@
-$(".dropdown").on("show.bs.dropdown",function(o){$(this).find(".dropdown-menu").first().stop(!0,!0).slideDown()}),$(".dropdown").on("hide.bs.dropdown",function(o){$(this).find(".dropdown-menu").first().stop(!0,!0).slideUp()});
-//# sourceMappingURL=maps/menu-slide.js.map
