b6a86c0f4514821fb1b6da2d1e74e4ee0951ffd7
retwedt <retwedt@gmail.com>
2016-03-21 16:56:55 -0500

    push new site live

---
+++ js/menu-slide.js
@@ -0,0 +1,2 @@
+$(".dropdown").on("show.bs.dropdown",function(o){$(this).find(".dropdown-menu").first().stop(!0,!0).slideDown()}),$(".dropdown").on("hide.bs.dropdown",function(o){$(this).find(".dropdown-menu").first().stop(!0,!0).slideUp()});
+//# sourceMappingURL=maps/menu-slide.js.map
---
+++ js/menu.js
@@ -0,0 +1,2 @@
+var winWidth=$(window).width();$(".nav-close").find("a").on("click",function(i){winWidth=$(window).width(),768>winWidth&&$(".navbar-collapse").collapse("toggle")}),$(".navbar-collapse").find(".dropdown-menu").find("a").on("click",function(i){winWidth=$(window).width(),768>winWidth&&$(".navbar-collapse").collapse("toggle")});
+//# sourceMappingURL=maps/menu.js.map
